31adab8d14b7297f68d2246c85a8307fd5769fa8
Your Name <you@example.com>
2021-12-04 18:04:14 +0800

    Filter is completely working, search by info added

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -9,23 +9,37 @@ class SearchController extends BaseSpaceTwigController {
 
         $type = isset($_GET['type']) ? $_GET['type'] : '';
         $title = isset($_GET['title']) ? $_GET['title'] : '';
+        $info =isset($_GET['info']) ? $_GET['info'] : '';
 
-        $sql = <<<EOL
+        if (isset($_GET['type'])){
+            if(($_GET['type'])=="All"){
+                $sql = <<<EOL
+SELECT id, title, image, description
+FROM titles
+WHERE (:title = '' OR title like CONCAT('%', :title, '%'))   
+    AND (:info = '' OR info like CONCAT('%', :info, '%'))
+                    
+EOL;
+            }else{
+                $sql = <<<EOL
 SELECT id, title, image, description
 FROM titles
 WHERE (:title = '' OR title like CONCAT('%', :title, '%'))        
+    AND (:info = '' OR info like CONCAT('%', :info, '%'))
     AND (type = :type)
 EOL;
+        }
+    
+            $query = $this->pdo->prepare($sql);
 
-        $query = $this->pdo->prepare($sql);
+            $query->bindValue("title", $title);
+            $query->bindValue("type", $type);
+            $query->bindValue("info",$info);
+            $query->execute();
 
-        $query->bindValue("title", $title);
-        $query->bindValue("type", $type);
-        $query->execute();
-
-        $context['objects'] = $query->fetchAll();
+            $context['objects'] = $query->fetchAll();
+        }
 
         return $context;
     }
-    
 }
