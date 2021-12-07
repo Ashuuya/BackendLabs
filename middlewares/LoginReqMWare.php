f6cfda5dcfa7e51cfd8ff4ffb71e4a61a7ec32bc
Your Name <you@example.com>
2021-12-07 17:51:00 +0800

    authorization is working via users database

--- middlewares/LoginReqMWare.php
+++ middlewares/LoginReqMWare.php
@@ -1,23 +1,29 @@
 <?php
 
-class LoginReqMWare extends BaseMIddleware {
+class LoginReqMWare extends BaseMiddleware{
     public function apply(BaseController $controller, array $context)
     {
-        // заводим переменные под правильный пароль
-        $valid_user = "password";
-        $valid_password = "user";
-        
-        // берем значения которые введет пользователь
-        $user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '';
-        $password = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
-        
-        // сверяем с корректными
-        if ($valid_user != $user || $valid_password != $password) {
-            // если не совпали, надо указать такой заголовок
-            // именно по нему браузер поймет что надо показать окно для ввода юзера/пароля
-            header('WWW-Authenticate: Basic realm="Titles"');
-            http_response_code(401); // ну и статус 401 -- Unauthorized, то есть неавторизован
-            exit; // прерываем выполнение скрипта
+        $query = $controller->pdo->prepare("SELECT username,password,id FROM users WHERE password= :my_password AND username= :my_username");
+
+        $query->bindValue("my_password", isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '');
+        $query->bindValue("my_username", isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '');
+        $query->execute();
+        $data = $query->fetch();
+
+        // $context['username'] = $data['username'];
+        // $context['password'] = $data['password'];
+
+         // заводим переменные под правильный пароль
+
+         // берем значения которые введет пользователь
+
+         // сверяем с корректными
+         if (!$data) {
+             // если не совпали, надо указать такой заголовок
+             // именно по нему браузер поймет что надо показать окно для ввода юзера/пароля
+             header('WWW-Authenticate: Basic realm="Titles"');
+             http_response_code(401); // ну и статус 401 -- Unauthorized, то есть неавторизован
+             exit; // прерываем выполнение скрипта
         }
     }
 }
